ceaa24151db7cc2e0db3000d31ad270da03d2f28
Sanya <example@gmail.com>
2023-02-07 18:39:45 +0200

    First commit

---
+++ js/sciprt.js
@@ -0,0 +1,79 @@
+'use strict';
+
+console.log('arr' + " - object");
+
+
+
+// console.log('hi' +'-me');
+// console.log(4 + 'letdown');
+
+
+// const category = 'toys';
+
+// console.log(`https://printscreen.com/${category}/5`);
+
+// const name = 'Ivan';
+
+// alert(`Hi, ${name} `);
+
+
+
+//alert('Hi everyone who is here with us');
+
+// const result = confirm('Are you here');
+// console.log(result);
+
+// const answer = +prompt("How old are you","");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name','');
+// answers[1] = prompt('What are doing here ','');
+// answers[2] = prompt('Who are you ','');
+
+// document.write (answers);
+
+
+
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//     abc: 'a',
+//     bcd: 'd',
+//     2: 'f'
+// };
+
+// arrObj['b'] = '222';
+
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Exsample of Syntacsyse objects with array
+//const storeName = 'My awesome store';
+
+//const storeDescription = {
+ //   budget: 10000,
+   // employees: ['Alex', 'John', 'Sam'],
+    //products: {
+      //  'soap': 20,
+        //'water': 5
+    //},
+   // open: true
+//}
