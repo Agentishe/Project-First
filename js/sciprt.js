28f8e3123ddd91ffe5674affaba97c4b7d06654a
Sanya <example@gmail.com>
2023-02-16 21:29:07 +0200

    For/While/do

--- js/sciprt.js
+++ js/sciprt.js
@@ -1,24 +1,63 @@
+"use strict";
 
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 2;
-
-// console.log(hamburger === 3 && fries  && cola );
-
-// if (hamburger === 3 && fries === 1 && cola === 1){
-//     console.log('Well done');
-//  } else {
-//     console.log(' We are leaving from here');
-// }
-
-const hamburger = 3;
-const fries = 0;
-const cola = 0;
-
-// console.log(hamburger === 3 && fries  && cola );
-
-if (hamburger === 2 || fries || cola ){
-    console.log('Well done');
- } else {
-    console.log(' We are glad to be with you');
+for (let  i = 0; i< 3; i++){
+    console.log(i);
+    for(let j= 0; j < 3; j++){
+        console.log(j);
+    }
 }
+
+//*
+//** 
+//*** 
+//****
+//*****
+//****** 
+
+let result = "";
+const lenthg = 7;
+
+for( let i = 1; i < lenthg; i++){
+
+    for (let j = 0; j < i; j++){
+        result += "*";
+    }
+    result += "\n";
+}
+console.log(result);
+
+start: for (let  i = 0; i< 3; i++){
+    console.log(`First level: ${i}`);
+    for(let j= 0; j < 2; j++){
+        console.log(`Second level: ${j}`);
+    }
+    for(let k= 0; k < 2; k++){
+        if (k === 2) continue start;
+        console.log(`Third level: ${k}`);
+    }
+}
+
+
+
+
+
+
+// let num = 50;
+
+// // while( num < 60){
+// //     console.log (num);
+// //     num++;
+// // }
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // } while (num < 53);
+
+// for ( let i = 1; i < 12; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
